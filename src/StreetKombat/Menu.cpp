36fbfcb4025b2c10fb6e8dfaa13eed03fa76f863
MatheusEliasC <matheus.elias.cruz@hotmail.com>
2021-05-28 16:07:34 -0300

    Projeto do menu

---
+++ src/StreetKombat/Menu.cpp
@@ -0,0 +1,27 @@
+// ----------------------------------------------------------------
+// From Game Programming in C++ by Sanjay Madhav
+// Copyright (C) 2017 Sanjay Madhav. All rights reserved.
+// 
+// Released under the BSD License
+// See LICENSE in root directory for full details.
+// ----------------------------------------------------------------
+
+// Header deste codigo
+#include "Menu.h"
+
+
+Menu::Menu()
+{
+
+}
+
+bool Menu::Initialize()
+{
+	return 1;
+}
+
+void Menu::RunLoop()
+{
+	
+}
+
